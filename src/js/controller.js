8e4ce6ddc4aab3b1c72e8f5189b1984b7b7cd221
Vlad <vladislav.stan.1998@gmail.com>
2023-05-26 14:37:37 +0300

    implemented pagination of the list of dishes

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,6 +2,7 @@ import * as model from '../js/model';
 import recipeView from '../js/views/recipeView';
 import searchView from '../js/views/searchView';
 import resultsView from '../js/views/resultsView';
+import paginationView from '../js/views/paginationView';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
@@ -33,12 +34,19 @@ const controlSearchResults = async () => {
 
         await model.loadSearchResults(query);
 
-        resultsView.render(model.state.search.results);
+        controlPagination();
     } catch (err) {}
+};
+
+const controlPagination = (goToPage = 1) => {
+    resultsView.render(model.getSearchResultsPage(goToPage));
+
+    paginationView.render(model.state.search);
 };
 
 const init = () => {
     recipeView.addHandlerRender(controlRecipes);
     searchView.addHandlerSearch(controlSearchResults);
+    paginationView.addHandlerClick(controlPagination);
 };
 init();
--- src/js/model.js
+++ src/js/model.js
@@ -1,4 +1,4 @@
-import { API_URL } from './config';
+import { API_URL, RES_PER_PAGE } from './config';
 import { getJSON } from './helpers';
 
 export const state = {
@@ -6,12 +6,14 @@ export const state = {
     search: {
         query: '',
         results: [],
+        page: 1,
+        resultsPerPage: RES_PER_PAGE,
     },
 };
 
 export const loadRecipe = async id => {
     try {
-        const data = await getJSON(`${API_URL}5ed6604591c37cdc054bc886`);
+        const data = await getJSON(`${API_URL}${id}`);
 
         const { recipe } = data.data;
         state.recipe = {
@@ -46,4 +48,13 @@ export const loadSearchResults = async query => {
     } catch (err) {
         throw err;
     }
+};
+
+export const getSearchResultsPage = (page = state.search.page) => {
+    state.search.page = page;
+
+    const start = (page - 1) * state.search.resultsPerPage;
+    const end = page * state.search.resultsPerPage;
+
+    return state.search.results.slice(start, end);
 };
