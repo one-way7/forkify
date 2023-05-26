651a973770947ecbaf990705f62a9d8e4a8709d1
Vlad <vladislav.stan.1998@gmail.com>
2023-05-26 10:19:16 +0300

    implemented fetch request by Id

--- src/js/model.js
+++ src/js/model.js
@@ -3,11 +3,15 @@ import { getJSON } from './helpers';
 
 export const state = {
     recipe: {},
+    search: {
+        query: '',
+        results: [],
+    },
 };
 
 export const loadRecipe = async id => {
     try {
-        const data = await getJSON(`${API_URL}/5ed6604591c37cdc054bc886`);
+        const data = await getJSON(`${API_URL}5ed6604591c37cdc054bc886`);
 
         const { recipe } = data.data;
         state.recipe = {
@@ -20,6 +24,25 @@ export const loadRecipe = async id => {
             cookingTime: recipe.cooking_time,
             ingredients: recipe.ingredients,
         };
+    } catch (err) {
+        throw err;
+    }
+};
+
+export const loadSearchResults = async query => {
+    try {
+        state.search.query = query;
+
+        const data = await getJSON(`${API_URL}?search=${query}`);
+
+        state.search.results = data.data.recipes.map(rec => {
+            return {
+                id: rec.id,
+                title: rec.title,
+                publisher: rec.publisher,
+                image: rec.image_url,
+            };
+        });
     } catch (err) {
         throw err;
     }
