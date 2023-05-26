faab309421810b527b4f5bfba917ddab91bb99ff
Vlad <vladislav.stan.1998@gmail.com>
2023-05-26 12:07:16 +0300

    wrote a function for getting and displaying a list of dishes

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,14 +1,22 @@
 import * as model from '../js/model';
 import recipeView from '../js/views/recipeView';
+import searchView from '../js/views/searchView';
+import resultsView from '../js/views/resultsView';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
+if (module.hot) {
+    module.hot.accept();
+}
+
 const controlRecipes = async () => {
     try {
+        const id = window.location.hash.slice(1);
+
         recipeView.renderSpinner();
 
-        await model.loadRecipe();
+        await model.loadRecipe(id);
 
         recipeView.render(model.state.recipe);
     } catch (err) {
@@ -16,7 +24,21 @@ const controlRecipes = async () => {
     }
 };
 
+const controlSearchResults = async () => {
+    try {
+        resultsView.renderSpinner();
+
+        const query = searchView.getQuery();
+        if (!query) return;
+
+        await model.loadSearchResults(query);
+
+        resultsView.render(model.state.search.results);
+    } catch (err) {}
+};
+
 const init = () => {
     recipeView.addHandlerRender(controlRecipes);
+    searchView.addHandlerSearch(controlSearchResults);
 };
 init();
