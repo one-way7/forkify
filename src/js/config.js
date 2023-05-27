beec1f9fdbc3cb2b386408c0cb29f02d0852e3da
Vlad <vladislav.stan.1998@gmail.com>
2023-05-27 13:26:46 +0300

    added an API KEY variable and a variable containing the closing time of the modal window

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = 'ed508f86-12b3-4aae-b41d-abc37106e5e7';
+export const MODAL_CLOSE_SEC = 2.5;
